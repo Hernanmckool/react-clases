b5f67a48519b0283c2c6aeb43713eb029ac89cfb
Hernan Perez <hernanperez@MacBook-Pro-de-Hernan.local>
2026-05-25 15:43:36 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Products from './components/Products.jsx';
+
+function App() {
+
+  return (
+    <>
+      <h1>Bienvenidos</h1>
+      <Products products="Tomate" />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Products.jsx
@@ -0,0 +1,5 @@
+const Products = (props) => {
+    return <h2>{props.products}</h2>;
+}
+
+export default Products;
